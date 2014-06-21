2bfaf5ef66efd8766ce9d49dd965938f25b4ca7b
teitei-tk <teitei.tk@gmail.com>
2014-06-21 15:02:54 +0900

    image tap move viewContorller

---
+++ SwiftTiqavViewer/DetailController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailController.swift
+//  SwiftTiqavViewer
+//
+//  Created by 比嘉 祥勝 on 2014/06/21.
+//  Copyright (c) 2014年 himara2. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+/*
+class DetailController: UIViewController, UITableViewDelegate, UITableViewDataSource, NSURLConnectionDelegate {
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
+*/
